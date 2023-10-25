688a30ccebee45099e143f9e1c6273e096c268f6
supersiq <jonathan.broms@gmail.com>
2023-10-25 17:51:43 +0200

    asdfadsgadsg

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,6 @@ export default function Home() {
   // A function to handle what happens when the user submits a form
   async function onSubmit(event) {
     event.preventDefault(); // Prevents the webpage from reloading when the form is submitted
-    alert('input was:', value);
     try {
       // Sends a message to the server to create something
       const response = await fetch("/api/generate", {
