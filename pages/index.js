f417cb81ed2c8e320e394ea29255cbebc70d6c96
supersiq <jonathan.broms@gmail.com>
2023-10-25 18:12:38 +0200

    e

--- pages/index.js
+++ pages/index.js
@@ -10,49 +10,39 @@ export default function Home() {
   const [result, setResult] = useState();               // For the generated result
   const [imageUrl, setImageUrl] = useState("/meme.png"); // Initialize with the default image URL
 
-  // A function to handle what happens when the user submits a form
-  async function onSubmit(event) {
-    event.preventDefault(); // Prevents the webpage from reloading when the form is submitted
-    try {
-      // Sends a message to the server to create something
-      const response = await fetch("/api/generate", {
-        method: "POST", // We're telling the server that we want to create something new
-        headers: {
-          "Content-Type": "application/json", // We're sending data in a specific way
-        },
-        body: JSON.stringify({ keyword: keywordInput }), // We're sending the user's input
-      });
-
-      // Reads the response from the server
-      const data = await response.json();
-
-      // Checks if there's a problem with the request
-      if (response.status !== 200) {
-        // If there's a problem, we show an error message
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-
-      // If everything goes well, we show the result and clear the input
-      setResult(data.result);
-      setKeywordInput("");
-    } catch (error) {
-      // If something goes wrong, we show an error message on the webpage
-      console.error(error);
-      alert(error.message);
-    }
-    const response = await openai.createImage({
-      prompt: "a white siamese cat",
-      n: 1,
-      size: "1024x1024",
+// A function to handle what happens when the user submits a form
+async function onSubmit(event) {
+  event.preventDefault();
+  try {
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ keyword: keywordInput }),
     });
-    image_url = response.data.data[0].url;    
+
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw data.error || new Error(`Request failed with status ${response.status}`);
+    }
+
+    // If everything goes well, we show the result and clear the input
+    setResult(data.result);
+    setKeywordInput("");
     
-    const imageResponse = await fetch(data.imageUrl);
-    const newImageUrl = imageResponse.url;
+    // The image URL is already provided by the server, no need to make an additional API call
+    const newImageUrl = data.imageUrl;
 
     // Update the image URL to the new one
-    setImageUrl(newImageUrl); 
+    setImageUrl(newImageUrl);
+  } catch (error) {
+    // If something goes wrong, we show an error message on the webpage
+    console.error(error);
+    alert(error.message);
   }
+}
 
   // The part of the webpage that you can see
   return (
